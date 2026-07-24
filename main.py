ecdf2733b660ec844dfddf5fed6ea95034235feb
bharat-apps <40429093+iamdecemberborn@users.noreply.github.com>
2026-07-25 04:50:25 +0530

    test
    test

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("Hello, World!")
 ##checking thhis line afterremoving Fine-grained personal access tokens
+
+
+import json
+
+def lambda_handler(event, context):
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from GitHub Actions and Terraform!')
+    }
+
