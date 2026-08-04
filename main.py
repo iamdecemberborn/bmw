4be68dd22337c17bb9acea83e69116965c416d22
iamdecemberborn <40429093+iamdecemberborn@users.noreply.github.com>
2026-08-04 23:49:30 +0530

    checking from HPP

--- main.py
+++ main.py
@@ -97,9 +97,5 @@ def lambda_handler(event, context):
         }
         
     except Exception as e:
-        print(f"Error processing file {key} from bucket {bucket}: {str(e)}")
-        
+        print(f"Error processing file {key} from bucket {bucket}: {str(e)}")        
         raise e
-
-
-
